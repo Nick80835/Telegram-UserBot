1af76d3d6375de1053504fab70de4c22dc3261ef
Nick Filmer <nick80835@gmail.com>
2019-09-01 10:29:36 -0400

    misc cleaning for init and sample config

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,35 +5,40 @@
 #
 """ Userbot initialization. """
 
-import os
+import os, pylast
 from distutils.util import strtobool as sb
-from logging import basicConfig, getLogger, INFO, DEBUG
-from sys import version_info, executable
-
-import pylast
 from dotenv import load_dotenv
+from logging import basicConfig, getLogger, INFO, DEBUG
 from requests import get
+from sys import version_info, executable
 from telethon import TelegramClient
 
-load_dotenv("config.env")
-
-# Command prefix
-CMDPREFIX = os.environ.get("CMDPREFIX", ".")
+getstr = lambda varname, default: os.environ.get(varname, default)
+getbool = lambda varname, default: sb(os.environ.get(varname, default))
+getint = lambda varname, default: int(os.environ.get(varname, default))
 
 try: BASEDIR
 except: BASEDIR = os.getcwd()
 
-# Bot Logs setup:
-CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+load_dotenv("config.env")
 
-if CONSOLE_LOGGER_VERBOSE:
-    basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=DEBUG
-    )
-else:
-    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                level=INFO)
+# Telegram client key/hash
+API_KEY = getstr("API_KEY", None)
+API_HASH = getstr("API_HASH", None)
+
+# Command prefix
+CMDPREFIX = getstr("CMDPREFIX", ".")
+
+# Bot logging setup
+CONSOLE_LOGGER_VERBOSE = getbool("CONSOLE_LOGGER_VERBOSE", "False")
+BOTLOG_CHATID = getint("BOTLOG_CHATID", "0")
+BOTLOG = getbool("BOTLOG", "False")
+
+basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=DEBUG if CONSOLE_LOGGER_VERBOSE else INFO
+)
+
 LOGS = getLogger(__name__)
 
 if version_info[0] < 3 or version_info[1] < 6:
@@ -41,58 +46,37 @@ if version_info[0] < 3 or version_info[1] < 6:
                " Multiple features depend on this. Bot quitting.")
     quit(1)
 
-# Check if the config was edited by using the already used variable
-CONFIG_CHECK = os.environ.get(
-    "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+# Autoban PMs, mute on welcome
+PM_AUTO_BAN = getbool("PM_AUTO_BAN", "False")
+WELCOME_MUTE = getbool("WELCOME_MUTE", "False")
 
-if CONFIG_CHECK:
-    LOGS.error("Please remove the line mentioned in the first \
-         hashtag from the config.env file")
-    quit(1)
+# Misc fancy integrations
+SCREENSHOT_LAYER_ACCESS_KEY = getstr("SCREENSHOT_LAYER_ACCESS_KEY", None)
+OPEN_WEATHER_MAP_APPID = getstr("OPEN_WEATHER_MAP_APPID", None)
+YOUTUBE_API_KEY = getstr("YOUTUBE_API_KEY", None)
+SPOTIFY_USERNAME = getstr("SPOTIFY_USERNAME", None)
+SPOTIFY_PASS = getstr("SPOTIFY_PASS", None)
+BIO_PREFIX = getstr("BIO_PREFIX", None)
+DEFAULT_BIO = getstr("DEFAULT_BIO", None)
+CURRENCY_API = getstr("CURRENCY_API", None)
+GDRIVE_FOLDER = getstr("GDRIVE_FOLDER", None)
 
-API_KEY = os.environ.get("API_KEY", None)
-API_HASH = os.environ.get("API_HASH", None)
-
-BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", "0"))
-BOTLOG = sb(os.environ.get("BOTLOG", "False"))
-
-PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
-
-CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
-SCREENSHOT_LAYER_ACCESS_KEY = os.environ.get("SCREENSHOT_LAYER_ACCESS_KEY",
-                                             None)
-
-OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-
-WELCOME_MUTE = sb(os.environ.get("WELCOME_MUTE", "False"))
-
-YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-
-SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME", None)
-SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
-BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
-LASTFM_API = os.environ.get("LASTFM_API", None)
-LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+# LastFM integration variables
+LASTFM_API = getstr("LASTFM_API", None)
+LASTFM_SECRET = getstr("LASTFM_SECRET", None)
+LASTFM_USERNAME = getstr("LASTFM_USERNAME", None)
+LASTFM_PASSWORD_PLAIN = getstr("LASTFM_PASSWORD", None)
 LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
-if not LASTFM_USERNAME == "None":
-    lastfm = pylast.LastFMNetwork(api_key=LASTFM_API,
-                                  api_secret=LASTFM_SECRET,
-                                  username=LASTFM_USERNAME,
-                                  password_hash=LASTFM_PASS)
+
+if LASTFM_USERNAME != 'None':
+    lastfm = pylast.LastFMNetwork(
+        api_key=LASTFM_API,
+        api_secret=LASTFM_SECRET,
+        username=LASTFM_USERNAME,
+        password_hash=LASTFM_PASS
+    )
 else:
     lastfm = None
-
-CURRENCY_API = os.environ.get("CURRENCY_API", None)
-
-GDRIVE_FOLDER = os.environ.get("GDRIVE_FOLDER", None)
-
-# pylint: disable=invalid-name
-bot = TelegramClient("userbot", API_KEY, API_HASH)
 
 if os.path.exists("learning-data-root.check"):
     os.remove("learning-data-root.check")
@@ -105,6 +89,8 @@ URL += 'databasescape/master/learning-data-root.check'
 with open('learning-data-root.check', 'wb') as load:
     load.write(get(URL).content)
 
+# pylint: disable=invalid-name
+bot = TelegramClient("userbot", API_KEY, API_HASH)
 
 # Global Variables
 COUNT_MSG = 0
@@ -115,116 +101,23 @@ LASTMSG = {}
 ENABLE_KILLME = True
 CMD_HELP = {}
 AFKREASON = "no reason"
-ZALG_LIST = [[
-    "̖",
-    " ̗",
-    " ̘",
-    " ̙",
-    " ̜",
-    " ̝",
-    " ̞",
-    " ̟",
-    " ̠",
-    " ̤",
-    " ̥",
-    " ̦",
-    " ̩",
-    " ̪",
-    " ̫",
-    " ̬",
-    " ̭",
-    " ̮",
-    " ̯",
-    " ̰",
-    " ̱",
-    " ̲",
-    " ̳",
-    " ̹",
-    " ̺",
-    " ̻",
-    " ̼",
-    " ͅ",
-    " ͇",
-    " ͈",
-    " ͉",
-    " ͍",
-    " ͎",
-    " ͓",
-    " ͔",
-    " ͕",
-    " ͖",
-    " ͙",
-    " ͚",
-    " ",
-],
-             [
-                 " ̍",
-                 " ̎",
-                 " ̄",
-                 " ̅",
-                 " ̿",
-                 " ̑",
-                 " ̆",
-                 " ̐",
-                 " ͒",
-                 " ͗",
-                 " ͑",
-                 " ̇",
-                 " ̈",
-                 " ̊",
-                 " ͂",
-                 " ̓",
-                 " ̈́",
-                 " ͊",
-                 " ͋",
-                 " ͌",
-                 " ̃",
-                 " ̂",
-                 " ̌",
-                 " ͐",
-                 " ́",
-                 " ̋",
-                 " ̏",
-                 " ̽",
-                 " ̉",
-                 " ͣ",
-                 " ͤ",
-                 " ͥ",
-                 " ͦ",
-                 " ͧ",
-                 " ͨ",
-                 " ͩ",
-                 " ͪ",
-                 " ͫ",
-                 " ͬ",
-                 " ͭ",
-                 " ͮ",
-                 " ͯ",
-                 " ̾",
-                 " ͛",
-                 " ͆",
-                 " ̚",
-             ],
-             [
-                 " ̕",
-                 " ̛",
-                 " ̀",
-                 " ́",
-                 " ͘",
-                 " ̡",
-                 " ̢",
-                 " ̧",
-                 " ̨",
-                 " ̴",
-                 " ̵",
-                 " ̶",
-                 " ͜",
-                 " ͝",
-                 " ͞",
-                 " ͟",
-                 " ͠",
-                 " ͢",
-                 " ̸",
-                 " ̷",
-                 " ͡",
-             ]]
+
+# Zalgo characters
+ZALG_LIST = [
+    [
+        "̖"," ̗"," ̘"," ̙"," ̜"," ̝"," ̞"," ̟"," ̠"," ̤"," ̥"," ̦",
+        " ̩"," ̪"," ̫"," ̬"," ̭"," ̮"," ̯"," ̰"," ̱"," ̲"," ̳"," ̹",
+        " ̺"," ̻"," ̼"," ͅ"," ͇"," ͈"," ͉"," ͍"," ͎"," ͓"," ͔"," ͕",
+        " ͖"," ͙"," ͚"," ",
+    ],
+    [
+        " ̍"," ̎"," ̄"," ̅"," ̿"," ̑"," ̆"," ̐"," ͒"," ͗"," ͑"," ̇",
+        " ̈"," ̊"," ͂"," ̓"," ̈́"," ͊"," ͋"," ͌"," ̃"," ̂"," ̌"," ͐",
+        " ́"," ̋"," ̏"," ̽"," ̉"," ͣ"," ͤ"," ͥ"," ͦ"," ͧ"," ͨ"," ͩ",
+        " ͪ"," ͫ"," ͬ"," ͭ"," ͮ"," ͯ"," ̾"," ͛"," ͆"," ̚",
+    ],
+    [
+        " ̕"," ̛"," ̀"," ́"," ͘"," ̡"," ̢"," ̧"," ̨"," ̴"," ̵"," ̶",
+        " ͜"," ͝"," ͞"," ͟"," ͠"," ͢"," ̸"," ̷"," ͡",
+    ]
+]
