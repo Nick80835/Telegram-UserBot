192bdb65e1150d25d3477ae59b23f3254d01de87
Nick Filmer <nick80835@gmail.com>
2019-10-27 09:51:50 -0400

    e n h a n c e reddit

--- userbot/modules/reddit.py
+++ userbot/modules/reddit.py
@@ -13,35 +13,21 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import io
-
 import praw
-import requests
 
 from userbot import CMD_HELP
 from userbot.events import errors_handler, register
 
-TOKEN_URL = "https://www.reddit.com/api/v1/access_token"
-
-REDDIT_URL = "https://reddit.com/r/{}/hot"
-HOT_PARAMS = {"g": "GLOBAL"}
-
-
-async def get_reddit():
-    reddit = praw.Reddit(client_id='-fmzwojFG6JkGg',
-                         client_secret=None,
-                         user_agent='TG_Userbot')
-
-    return reddit
+REDDIT = praw.Reddit(client_id='-fmzwojFG6JkGg',
+                     client_secret=None,
+                     user_agent='TG_Userbot')
 
 
 @register(outgoing=True, pattern="suffer")
 @errors_handler
 async def makemesuffer(event):
-    reddit = await get_reddit()
-
     for _ in range(10):
-        post = reddit.subreddit('makemesuffer').random()
+        post = REDDIT.subreddit('makemesuffer').random()
 
         if post.url:
             image_url = post.url
@@ -53,6 +39,72 @@ async def makemesuffer(event):
 
     try:
         await event.client.send_file(event.chat_id, image_url)
+        await event.delete()
+    except:
+        await event.edit("`Failed to download content!`")
+
+
+@register(outgoing=True, pattern="snafu")
+@errors_handler
+async def coaxedintoasnafu(event):
+    for _ in range(10):
+        post = REDDIT.subreddit('coaxedintoasnafu').random()
+
+        if post.url:
+            image_url = post.url
+            break
+
+    if not image_url:
+        await event.edit("`Failed to find any valid content!`")
+        return
+
+    try:
+        await event.client.send_file(event.chat_id, image_url)
+        await event.delete()
+    except:
+        await event.edit("`Failed to download content!`")
+
+
+@register(outgoing=True, pattern="aita")
+@errors_handler
+async def amitheasshole(event):
+    for _ in range(10):
+        post = REDDIT.subreddit('amitheasshole').random()
+
+        if post.title:
+            title_text = post.title
+            break
+
+    if not title_text:
+        await event.edit("`Failed to find any valid content!`")
+        return
+
+    await event.reply(title_text)
+    await event.delete()
+
+
+@register(outgoing=True, pattern="jon(x|)")
+@errors_handler
+async def imsorryjon(event):
+    if "x" in event.pattern_match.group(0):
+        sub = "imreallysorryjon"
+    else:
+        sub = "imsorryjon"
+
+    for _ in range(10):
+        post = REDDIT.subreddit(sub).random()
+
+        if post.url:
+            image_url = post.url
+            break
+
+    if not image_url:
+        await event.edit("`Failed to find any valid content!`")
+        return
+
+    try:
+        await event.client.send_file(event.chat_id, image_url)
+        await event.delete()
     except:
         await event.edit("`Failed to download content!`")
 
@@ -61,4 +113,19 @@ CMD_HELP.update({
     'suffer':
     ".suffer"
     "\nUsage: Suffer."
+})
+CMD_HELP.update({
+    'snafu':
+    ".snafu"
+    "\nUsage: Coaxed into a snafu."
+})
+CMD_HELP.update({
+    'aita':
+    ".aita"
+    "\nUsage: Am I the asshole?"
+})
+CMD_HELP.update({
+    'jon':
+    ".jon(x)"
+    "\nUsage: I'm (really) sorry Jon."
 })
